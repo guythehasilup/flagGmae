8eb6407d3f64c0c1037369835ea82a96530194da
bubble <guyyan12@gmail.com>
2023-08-22 10:23:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import game_field
+
+
+def main():
+    game_field.initiate_empty_game_field()
+    game_field.initiate_game_field()
+    game_field.print_board()
+
+
+if __name__ == "__main__":
+    main()
