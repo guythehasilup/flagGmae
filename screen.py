aaf3f51988b0f87347f1356b093e71ffc7f6df22
bubble <guyyan12@gmail.com>
2023-08-22 11:49:46 +0300

    Initial commit

--- screen.py
+++ screen.py
@@ -10,13 +10,22 @@ def draw_game():
     pygame.display.flip()
 
 
+def draw_night_rect(x, y):
+    for i in range(4):
+        pygame.draw.rect(screen, consts.BORDER_COLOR,
+                         (x, y, consts.SQUARE_EDGE, consts.SQUARE_EDGE), 1)
+
+
 def draw_night_vision_game():
     # pygame.draw.rect(screen, consts.NIGHT_VISION_BACKGROUND,
     # (consts.SQUARE_EDGE, consts.SQUARE_EDGE, consts.SQUARE_EDGE, consts.SQUARE_EDGE), 0)
-
-    for i in range(4):
-        pygame.draw.rect(screen, consts.BORDER_COLOR,
-                         (consts.SQUARE_EDGE, consts.SQUARE_EDGE, consts.SQUARE_EDGE, consts.SQUARE_EDGE), 1)
+    y = 0
+    for row in range(consts.MATRIX_HEIGHT):
+        x = 0
+        for col in range(consts.MATRIX_WIDTH):
+            draw_night_rect(x, y)
+            x += consts.SQUARE_EDGE
+        y += consts.SQUARE_EDGE
     pygame.display.flip()
 
 
