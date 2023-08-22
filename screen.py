719d167773d922a95d8cf0d19ae78b0d2ec647ab
bubble <guyyan12@gmail.com>
2023-08-22 12:30:33 +0300

    drawing soldier(get center_x, center_y)

--- screen.py
+++ screen.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
 def draw_game():
     screen.fill(consts.BACKGROUND_COLOR)
+    draw_soldier(0,0)
     pygame.display.flip()
 
 
@@ -17,8 +18,6 @@ def draw_night_rect(x, y):
 
 
 def draw_night_vision_game():
-    # pygame.draw.rect(screen, consts.NIGHT_VISION_BACKGROUND,
-    # (consts.SQUARE_EDGE, consts.SQUARE_EDGE, consts.SQUARE_EDGE, consts.SQUARE_EDGE), 0)
     y = 0
     for row in range(consts.MATRIX_HEIGHT):
         x = 0
@@ -26,11 +25,20 @@ def draw_night_vision_game():
             draw_night_rect(x, y)
             x += consts.SQUARE_EDGE
         y += consts.SQUARE_EDGE
+    draw_night_soldier(0,0)
     pygame.display.flip()
 
 
-def draw_soldier():
-    pass
+def draw_soldier(x, y):
+    sized_soldier = pygame.transform.scale(consts.SOLDIER_IMAGE, (
+        consts.SQUARE_EDGE * consts.SOLDIER_WIDTH, consts.SQUARE_EDGE * consts.SOLDIER_HEIGHT))
+    screen.blit(sized_soldier, (x, y))
+
+
+def draw_night_soldier(x, y):
+    sized_soldier = pygame.transform.scale(consts.NIGHT_SOLDIER_IMAGE, (
+        consts.SQUARE_EDGE * consts.SOLDIER_WIDTH, consts.SQUARE_EDGE * consts.SOLDIER_HEIGHT))
+    screen.blit(sized_soldier, (x, y))
 
 
 def draw_flag():
